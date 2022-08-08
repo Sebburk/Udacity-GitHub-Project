55c4d385edb0f2a82f7bc4965250ce6ed622c877
Sebburk <110697287+Sebburk@users.noreply.github.com>
2022-08-08 16:41:40 -0700

    Editing ending message

--- bikeshare.py
+++ bikeshare.py
@@ -217,7 +217,7 @@ def main():
         user_stats(df,city)
         display_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to try again? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
@@ -235,7 +235,7 @@ def main():
         user_stats(df,city)
         display_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to try again? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break       
         
